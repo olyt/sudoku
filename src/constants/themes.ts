cb300b992733faf6927d65ed8b2303c2c89d80c6
Oleksandr Lytovchenko <olyt54@gmail.com>
2023-03-07 11:39:31 +0200

    add styled theme

---
+++ src/constants/themes.ts
@@ -0,0 +1,24 @@
+// first color palette
+// const darkSalmon = '#E29578';
+const unbleachedSilk = '#FFDDD2';
+const aliceBlue = '#EDF6F9';
+const middleBlueGreen = '#83C5BE';
+const ming = '#006D77';
+
+// another color palette
+// const lightBlue = '#B8D8D8';
+const cadetGray = '#7A9E9F';
+// const paynesGray = '#4F6367';
+// const beige = '#EEF5DB';
+const bittersweet = '#FE5F55';
+
+const defaultTheme = {
+  primary: ming,
+  secondary: middleBlueGreen,
+  secondaryLight: cadetGray,
+  primaryLight: aliceBlue,
+  error: bittersweet,
+  lightError: unbleachedSilk,
+};
+
+export default defaultTheme;
--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,15 @@ import './reset.css';
 import './index.css';
 import App from './App';
 import { AppContextProvider } from './context/AppContext';
+import { ThemeProvider } from 'styled-components';
+import defaultTheme from './constants/themes';
 
 ReactDOM.render(
   <React.StrictMode>
     <AppContextProvider>
-      <App />
+      <ThemeProvider theme={defaultTheme}>
+        <App />
+      </ThemeProvider>
     </AppContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
