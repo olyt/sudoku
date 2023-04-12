65cafa9a50b8632fd3478e4bb18d19c010e125be
Oleksandr Lytovchenko <olyt54@gmail.com>
2023-04-12 20:28:55 +0300

    add very basic styles for desktop

--- src/components/Buttons/HistoryButton.tsx
+++ src/components/Buttons/HistoryButton.tsx
@@ -2,7 +2,17 @@ import styled from 'styled-components';
 import BasicButton from './BasicButton';
 
 const HistoryButton = styled(BasicButton)`
-  height: 20px;
+  padding: 10px 15px;
+  box-sizing: border-box;
+  border: 1px solid ${({ theme }) => theme.primary};
+  color: ${({ theme }) => theme.primary};
+  border-radius: 5px;
+
+  &:hover {
+    border: none;
+    background: ${({ theme }) => theme.primary};
+    color: ${({ theme }) => theme.secondary};
+  }
 `;
 
 export default HistoryButton;
--- src/components/History/History.tsx
+++ src/components/History/History.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import HistoryButton from '../Buttons/HistoryButton';
+import styled from 'styled-components';
+
+const HistoryWrapper = styled.div`
+  height: 100px;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 15%;
+  margin: auto;
+  display: flex;
+  gap: 10px;
+`;
 
 const History: React.FC = () => {
   return (
-    <div>
+    <HistoryWrapper>
       <HistoryButton>back</HistoryButton>
       <HistoryButton>forward</HistoryButton>
-    </div>
+    </HistoryWrapper>
   );
 };
 
