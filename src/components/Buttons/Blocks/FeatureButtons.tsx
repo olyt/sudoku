32ca5c452ee0b089fd56aabc5218c6650b64e6f2
Oleksandr Lytovchenko <olyt54@gmail.com>
2023-05-05 17:31:35 +0300

    add hint button

--- src/components/Buttons/Blocks/FeatureButtons.tsx
+++ src/components/Buttons/Blocks/FeatureButtons.tsx
@@ -2,17 +2,23 @@ import React, { MouseEventHandler } from 'react';
 import { useAppContext } from '../../../context/AppContext';
 import { tryToUndo } from '../../../context/history/operations';
 import { ReactComponent as UndoIcon } from '../../../assets/svg/undo.svg';
+import { ReactComponent as HintIcon } from '../../../assets/svg/hint.svg';
 import FeatureButton from '../FeatureButton';
 
 const FeatureButtons: React.FC = () => {
-  const { dispatch, history } = useAppContext();
+  const { dispatch, history, hints } = useAppContext();
 
   const handleUndo: MouseEventHandler<HTMLButtonElement> = () => {
     dispatch(tryToUndo());
   };
 
+  const handleHint: MouseEventHandler<HTMLButtonElement> = () => {
+    console.log('hint');
+  };
+
   return (
     <>
+      <FeatureButton handler={handleHint} icon={HintIcon} error={!hints} />
       <FeatureButton
         handler={handleUndo}
         icon={UndoIcon}
