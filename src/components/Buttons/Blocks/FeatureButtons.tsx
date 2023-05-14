50918e2ee80a035da87df6cca24d7c2764563704
Oleksandr Lytovchenko <olyt54@gmail.com>
2023-05-14 14:52:20 +0300

    prevent using hints when game not started

--- src/components/Buttons/Blocks/FeatureButtons.tsx
+++ src/components/Buttons/Blocks/FeatureButtons.tsx
@@ -5,19 +5,22 @@ import { ReactComponent as UndoSVG } from '../../../assets/svg/undo.svg';
 import { ReactComponent as HintSVG } from '../../../assets/svg/hint.svg';
 import FeatureButton, { getIcon } from '../FeatureButton';
 import { hint } from '../../../context/hints/operations';
+import { EGameStatus } from '../../../context/types';
 
 const UndoIcon = getIcon(UndoSVG);
 const HintIcon = getIcon(HintSVG);
 
 const FeatureButtons: React.FC = () => {
-  const { dispatch, history, hints } = useAppContext();
+  const { dispatch, history, hints, gameInfo } = useAppContext();
 
   const handleUndo: MouseEventHandler<HTMLButtonElement> = () => {
     dispatch(tryToUndo());
   };
 
   const handleHint: MouseEventHandler<HTMLButtonElement> = () => {
-    dispatch(hint());
+    if (gameInfo.gameStatus === EGameStatus.InProgress) {
+      dispatch(hint());
+    }
   };
 
   return (
