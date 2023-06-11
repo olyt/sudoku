3872e3c368a7aae7fbbbdd16a6f0cc3c418b32e6
Oleksandr Lytovchenko <olyt54@gmail.com>
2023-06-11 08:50:26 +0300

    change generating buttons to work with new difficulties type

--- src/components/DifficultyBlock/DifficultyBlock.tsx
+++ src/components/DifficultyBlock/DifficultyBlock.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { DIFFICULTIES } from '../../utils/generateBoard';
-import { IDifficulties } from '../../types/types';
+import { EDifficulties } from '../../types/types';
 import { useAppContext } from '../../context/AppContext';
 import { startGame } from '../../context/operations';
 import { EModalComponents } from '../../context/types';
@@ -10,25 +9,20 @@ import { setModalComponent, setModalIsOpen } from '../../context/modal/actions';
 const DifficultyBlock: React.FC = () => {
   const { dispatch } = useAppContext();
 
-  const startNewGame: (difficulty: keyof IDifficulties) => void = (
-    difficulty
-  ) => {
+  const startNewGame: (difficulty: EDifficulties) => void = (difficulty) => {
     dispatch(startGame(difficulty));
     dispatch(setModalIsOpen(false));
     dispatch(setModalComponent(EModalComponents.Empty));
   };
 
   const generateButtons = (): JSX.Element[] => {
-    return Object.keys(DIFFICULTIES).map((difficulty) => {
+    return Object.values(EDifficulties).map((difficulty) => {
       const text = difficulty
         .charAt(0)
         .toUpperCase()
         .concat(difficulty.substring(1));
       return (
-        <ModalButton
-          key={difficulty}
-          onClick={() => startNewGame(difficulty as keyof IDifficulties)}
-        >
+        <ModalButton key={difficulty} onClick={() => startNewGame(difficulty)}>
           {text}
         </ModalButton>
       );
