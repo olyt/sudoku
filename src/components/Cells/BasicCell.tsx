a23d4cb4545c482bbbbad1fff1ffc551be2184d6
Oleksandr Lytovchenko <olyt54@gmail.com>
2026-02-07 12:46:01 +0100

    cells refactoring and aligning with current eslint settings

--- src/components/Cells/BasicCell.tsx
+++ src/components/Cells/BasicCell.tsx
@@ -1,19 +1,19 @@
 import styled from 'styled-components';
 
 const BasicCell = styled.div`
-  font-size: 25px;
-  box-sizing: border-box;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  aspect-ratio: 1 / 1;
-  background: ${({ theme }) => theme.primaryLight};
-  cursor: pointer;
+    font-size: 25px;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    aspect-ratio: 1 / 1;
+    background: ${({ theme }) => theme.primaryLight};
+    cursor: pointer;
 
-  &:hover {
-    background: ${({ theme }) => theme.primary};
-    color: ${({ theme }) => theme.primaryLight};
-  }
+    &:hover {
+        background: ${({ theme }) => theme.primary};
+        color: ${({ theme }) => theme.primaryLight};
+    }
 `;
 
 export default BasicCell;
