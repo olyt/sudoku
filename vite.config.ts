d20872978954629dc5b16bf8c7d47dd7513bcccd
Oleksandr Lytovchenko <olyt54@gmail.com>
2026-03-22 17:18:22 +0100

    added vite config

---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+
+export default defineConfig({
+    base: '/sudoku/',
+    plugins: [react(), svgr()],
+    test: {
+        environment: 'jsdom',
+        globals: true,
+        setupFiles: ['./src/setupTests.ts'],
+        coverage: {
+            provider: 'v8',
+            include: ['src/**/*.{ts,tsx}'],
+            exclude: [
+                'src/react-app-env.d.ts',
+                'src/index.tsx',
+                'src/App.tsx',
+                'src/GlobalStyles.ts',
+                'src/vite-env.d.ts',
+                'src/@types/**',
+                'src/theming/**',
+                'src/components/**',
+                'src/hooks/**',
+                'src/constants/**',
+                'src/context/AppContext.tsx',
+                'src/context/mainReducer.ts',
+                'src/context/state.ts',
+                'src/context/types.ts',
+                'src/utils/svgHelper.ts',
+            ],
+            reporter: ['text', 'html'],
+            thresholds: { lines: 100, functions: 100, branches: 100, statements: 100 },
+        },
+    },
+});
